e80b6b2d32b7a7aa7e171fb1d678de66e523404a
dmitrii <polkovnikov13@gmail.com>
2023-02-05 20:29:40 +0100

    Add parameters

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,10 +2,19 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "app", mixinStandardHelpOptions = true, version = "app.0.1")
 public class App implements Callable<Integer> {
+
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private File filepath1;
+    @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private File filepath2;
+
+    @CommandLine.Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish", description = "output format [default: stylish]")
+    private String format;
 
     @Override
     public Integer call() throws Exception {
