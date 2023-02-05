9fb6df43460300d5a629ac13f4fc6776763cd257
dmitrii <polkovnikov13@gmail.com>
2023-02-05 20:24:03 +0100

    integrate picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "app", mixinStandardHelpOptions = true, version = "app.0.1")
+public class App implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
---
+++ app/src/main/java/hexlet/code/Options.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+
+import picocli.CommandLine;
+
+public class Options {
+
+
+}
