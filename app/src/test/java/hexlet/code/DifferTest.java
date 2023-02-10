cd1e0ab845ecce28d1d843c898a93609f0418701
dmitrii <polkovnikov13@gmail.com>
2023-02-10 17:37:09 +0100

    Add workflow

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
 
