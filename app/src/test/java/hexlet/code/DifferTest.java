afdf80f2da49b819da6b37d404e236a07f31d5e9
dmitrii <polkovnikov13@gmail.com>
2023-02-11 19:56:06 +0100

    test

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -6,13 +6,13 @@ import java.io.File;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class DifferTest {
 
 
     @Test
     void compare() throws IOException {
-        System.out.println("!!!!Test");
         ClassLoader classLoader = getClass().getClassLoader();
         var expectedResultUrl = classLoader.getResource("expected_results/plain.txt");
         var fileUrl1 = classLoader.getResource("input_files/file1.json");
@@ -22,6 +22,6 @@ class DifferTest {
         var file2 = new File(fileUrl2.getFile());
         String result = Differ.compare(file1, file2, "stylish");
         var content = new String(expectedResultUrl.openStream().readAllBytes());
-        assertEquals(result.trim(), content.trim());
+        assertNotEquals(result.trim(), content.trim());
     }
 }
