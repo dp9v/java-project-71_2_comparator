aabcce044c6cd727ee9168b454e0c2abaea713fd
dmitrii <polkovnikov13@gmail.com>
2023-04-23 13:51:44 +0200

    review test fixes

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,25 +1,77 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.skyscreamer.jsonassert.JSONAssert;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
+    private static String expectedResultJson;
+    private static String expectedResultPlain;
+    private static String expectedResultStylish;
 
+    public static Path getAbsolutePath(String fileName) {
+        return Paths.get("./src/test/resources/", fileName).toAbsolutePath().normalize();
+    }
 
-    @Test
-    void compare() throws Exception {
-        ClassLoader classLoader = getClass().getClassLoader();
-        var expectedResultUrl = classLoader.getResource("expected_results/plain.txt");
-        var expectedContent = Files.readString(Path.of(expectedResultUrl.toURI()));
+    public static String readFile(String fileName) throws IOException {
+        Path filePath = getAbsolutePath(fileName);
+        return Files.readString(filePath).trim();
+    }
 
-        var fileFullPath1 = classLoader.getResource("input_files/json/file1.json").getPath();
-        var fileFullPath2 = classLoader.getResource("input_files/json/file2.json").getPath();
+    @BeforeAll
+    public static void setup() throws IOException {
+        expectedResultJson = readFile("expected_results/json.json");
+        expectedResultPlain = readFile("expected_results/plain.txt");
+        expectedResultStylish = readFile("expected_results/stylish.txt");
+    }
 
-        String result = Differ.generate(fileFullPath1, fileFullPath2);
-        assertEquals(result.trim(), expectedContent.trim());
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    public void testGenerateWithDefaultOutput(String inputFormat) throws Exception {
+        var file1 = getAbsolutePath("input_files/file1." + inputFormat).toString();
+        var file2 = getAbsolutePath("input_files/file2." + inputFormat).toString();
+
+        String actualResult = Differ.generate(file1, file2);
+        assertEquals(actualResult, expectedResultStylish);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    public void testGenerateWithStylishOutput(String inputFormat) throws Exception {
+        var file1 = getAbsolutePath("input_files/file1." + inputFormat).toString();
+        var file2 = getAbsolutePath("input_files/file2." + inputFormat).toString();
+
+        String actualResult = Differ.generate(file1, file2, "stylish");
+        assertEquals(actualResult, expectedResultStylish);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    public void testGenerateWithPlainOutput(String inputFormat) throws Exception {
+        var file1 = getAbsolutePath("input_files/file1." + inputFormat).toString();
+        var file2 = getAbsolutePath("input_files/file2." + inputFormat).toString();
+
+        String actualResult = Differ.generate(file1, file2, "plain");
+        assertEquals(actualResult, expectedResultPlain);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    public void testGenerateWithJsonOutput(String inputFormat) throws Exception {
+        var file1 = getAbsolutePath("input_files/file1." + inputFormat).toString();
+        var file2 = getAbsolutePath("input_files/file2." + inputFormat).toString();
+
+        String actualResult = Differ.generate(file1, file2, "json");
+
+        JSONAssert.assertEquals(expectedResultJson, actualResult, false);
+
     }
 }
